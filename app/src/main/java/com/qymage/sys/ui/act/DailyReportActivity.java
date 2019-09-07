6e03959775d95e035fb394672e1df800edf0e91b
LiaoChengzhi <1982879273@qq.com>
2019-09-08 01:20:15 +0800

    增加日报、月报两个页面布局

---
+++ app/src/main/java/com/qymage/sys/ui/act/DailyReportActivity.java
@@ -0,0 +1,15 @@
+package com.qymage.sys.ui.act;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qymage.sys.R;
+
+public class DailyReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_report);
+    }
+}
---
+++ app/src/main/java/com/qymage/sys/ui/act/MonthReportActivity.java
@@ -0,0 +1,15 @@
+package com.qymage.sys.ui.act;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qymage.sys.R;
+
+public class MonthReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_report);
+    }
+}
